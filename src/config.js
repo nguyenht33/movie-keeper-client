b8c32d7d90db21949d7ff2d2739a8fbd1b45ec5f
Hung Nguyen <nguyenht33@gmail.com>
2018-08-20 11:36:06 -0400

    changed export in config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
-module.exports = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
-  API_KEY: process.env.REACT_APP_MOVIE_API_KEY,
-  MOVIE_URL: 'https://api.themoviedb.org/3',
-  BACKDROP_URL: 'https://image.tmdb.org/t/p/w780',
-  THUMBNAIL_URL: 'https://image.tmdb.org/t/p/w154'
-}
+export const API_BASE_URL =
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+
+export const API_KEY =
+  API_KEY: process.env.REACT_APP_MOVIE_API_KEY;
+
+export const MOVIE_URL =
+  MOVIE_URL: 'https://api.themoviedb.org/3';
+
+export const BACKDROP_URL =
+  BACKDROP_URL: 'https://image.tmdb.org/t/p/w780';
+
+export const THUMBNAIL_URL =
+  THUMBNAIL_URL: 'https://image.tmdb.org/t/p/w154';
