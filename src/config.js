e9de5867abda13878b66c255ad64a35f135ec466
Hung Nguyen <nguyenht33@gmail.com>
2018-08-20 11:41:54 -0400

    fixed spelling err

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  rocess.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
 export const API_KEY =
   process.env.REACT_APP_MOVIE_API_KEY;
