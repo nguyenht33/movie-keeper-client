1f75b78108521de15c0db3dbe80872d7069c8395
Hung Nguyen <nguyenht33@gmail.com>
2018-08-20 11:39:58 -0400

    fixed export syntax

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 export const API_BASE_URL =
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+  rocess.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
 
 export const API_KEY =
-  API_KEY: process.env.REACT_APP_MOVIE_API_KEY;
+  process.env.REACT_APP_MOVIE_API_KEY;
 
 export const MOVIE_URL =
-  MOVIE_URL: 'https://api.themoviedb.org/3';
+  'https://api.themoviedb.org/3';
 
 export const BACKDROP_URL =
-  BACKDROP_URL: 'https://image.tmdb.org/t/p/w780';
+  'https://image.tmdb.org/t/p/w780';
 
 export const THUMBNAIL_URL =
-  THUMBNAIL_URL: 'https://image.tmdb.org/t/p/w154';
+  'https://image.tmdb.org/t/p/w154';
