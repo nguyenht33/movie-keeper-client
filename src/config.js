30faa1e0d1fb20e7cb5a28ac50035ea0e809b9c8
Hung Nguyen <nguyenht33@gmail.com>
2018-08-20 07:29:21 -0400

    upated api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
   API_KEY: process.env.REACT_APP_MOVIE_API_KEY,
   MOVIE_URL: 'https://api.themoviedb.org/3',
   BACKDROP_URL: 'https://image.tmdb.org/t/p/w780',
